80c6f09aea1ad8cce290e5a3585b8b12df280a63
root <elrayah.alsanhory@gmail.com>
2023-09-07 14:24:03 -0700

    printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - the main entery for the program
+ * print message to screen
+ * Return: 0 on sucess
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
