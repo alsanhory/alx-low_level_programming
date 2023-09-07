396540da4602baf4b6078d6643979b7b155404cd
root <elrayah.alsanhory@gmail.com>
2023-09-07 14:00:35 -0700

    modification for betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,11 @@
 *You are not allowed to use printf
 *Your program should end with the value 0
 */
+/*
+ * main the enter for the program
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
