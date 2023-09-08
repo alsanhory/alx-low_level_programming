9c2efe34ede6e92d5bf58ccc10dac50ccecc814b
root <elrayah.alsanhory@gmail.com>
2023-09-08 16:42:33 -0700

    print hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - the main entery of the program
+ * Return: 0 on sucess
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
