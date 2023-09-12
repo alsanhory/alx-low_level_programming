ccf0b58af1f05aafb1443f41c7d108838ce4ebf6
root <elrayah.alsanhory@gmail.com>
2023-09-12 15:12:53 -0700

    first task of print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - the entry for the program
+ * Return: 0 on sucess
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
