5f894cb8dad16920d5c218079965a3514b57db99
root <elrayah.alsanhory@gmail.com>
2023-09-12 17:01:58 -0700

    check the letter if it is lower or capital

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+/**
+ * _putchar - function to use putchar
+ * Return: character
+ */
+int _putchar(char c)
+{
+	return putchar(c);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 #include<stdio.h>
-int _putchar(void);
+int _putchar(char);
 int print_alphabet(void);
 int print_alphabet_x10(void);
+int _islower(int c);
