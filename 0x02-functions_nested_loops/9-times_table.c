07aa9d85cb5cd01df73a49467705167f20ee8b97
root <elrayah.alsanhory@gmail.com>
2023-09-12 19:51:09 -0700

    table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ * times_table - to make addition
+ * Return: the result of sum
+ */
+void times_table(void)
+{
+	int i;
+
+	for(i = 0; i <= 9; i++)
+	{
+		for (j =0; j <= 9; j++)
+		{
+			res = i * j;
+			puthcar(res + '0');
+			if (j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		putchar('\n');
+	}
+}
