2b8f5eff5d28c24fe6ab9e1d1b945821fefee190
root <elrayah.alsanhory@gmail.com>
2023-09-12 19:25:18 -0700

    adition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * add - function to check if character is minus or pls
+ * @n: the character to check
+ * Return: 0 always
+ */
+int add(int a, int b)
+{
+	int thelast = a + b;
+
+	return (thelast);
+}
