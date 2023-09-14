4f10f30f57d687649888f7de33b482d2fe8b27f3
root <elrayah.alsanhory@gmail.com>
2023-09-14 12:24:57 -0700

    commiting first exersiz

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-#include "main.h"
-
-/**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
-
-int main(void)
-{
-        int i;
-
-        i = 98;
-        positive_or_negative(i);
-
-        return (0);
-=======
-#include <time.h>
-/* more headers goes there */
 #include <stdio.h>
 /* betty style doc for function main goes there */
 /**
@@ -44,5 +25,4 @@ int main(void)
 		printf("%d is zero\n", n);
 	}
 	return (0);
->>>>>>> ed6f8a1d34563c7085c94179a0b58f7e5a731f95
 }
