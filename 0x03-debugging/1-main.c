81735709df3b46d5e607e6e732c9ddba1d84d53f
root <elrayah.alsanhory@gmail.com>
2023-09-14 10:27:41 -0700

    mergin files

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,11 +13,7 @@ int main(void)
 
         i = 0;
 
-<<<<<<< HEAD
-        while (i < 10)
-=======
        /* while (i < 10)*/
->>>>>>> ed6f8a1d34563c7085c94179a0b58f7e5a731f95
         {
                 putchar(i);
         }
