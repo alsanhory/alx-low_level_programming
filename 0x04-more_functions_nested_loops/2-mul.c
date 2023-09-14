76d0ab05267555c5558c9f68644afec1952d4de9
root <elrayah.alsanhory@gmail.com>
2023-09-14 15:24:45 -0700

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * mul - function to check if character is digit
+ * @a: first number
+ * @b: second number
+ * Return: the result always
+ */
+int mul(int a, int b)
+{
+	int res;
+
+	res = a * b;
+	return (res);
+}
