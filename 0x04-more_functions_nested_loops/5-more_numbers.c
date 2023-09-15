2210e7788a7590fda2e24487dd93b98debff5eda
root <elrayah.alsanhory@gmail.com>
2023-09-14 19:33:17 -0700

    trying more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,21 +5,21 @@
  */
 void more_numbers(void)
 {
-	int c, i;
-
+	int i,c;
 	for (i = 0; i < 10; i++)
 	{
 		for (c = 0; c <= 14; c++)
 		{
-			if (c >= 10)
+			if (c > 9)
 			{
-				putchar((c/10) + '0');
-				c %= 10;
+				putchar(c / 10 + '0'); 
+				putchar(c % 10 + '0'); 
 			}
-
-			putchar(c + '0');
+			else
+			{
+				putchar(c + '0');  
+			}
 		}
 		putchar('\n');
 	}
-	putchar('\n');
 }
