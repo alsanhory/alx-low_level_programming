9b3d73a4806131760b48060dabd7f3ba345bc268
root <elrayah.alsanhory@gmail.com>
2023-09-18 15:22:17 -0700

    reverse function

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * rev_string - the entry for the program
+ * @s: pointer to variable to change
+ * Return: the number of len on sucess
+ */
+void rev_string(char *s)
+{
+	int len = 0, i;
+	char c;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	for (i = 0; i < len / 2; i++)
+	{
+		c = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = c;
+		len--;
+	}
+}
