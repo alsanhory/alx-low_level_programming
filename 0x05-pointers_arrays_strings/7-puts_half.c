b2fb68f2bc6c3ff7d9c3b4014ae1c352f0921b2f
root <elrayah.alsanhory@gmail.com>
2023-09-18 15:57:54 -0700

    print other half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * puts_half - the entry for the program
+ * @str: pointer to variable to change
+ * Return: the number of len on sucess
+ */
+void puts_half(char *str)
+{
+	int len = 0, i, starting;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	if (len % 2 == 0)
+	{
+		starting = len / 2;
+	}
+	else
+	{
+		starting = (len - 1) / 2;
+	}
+	for (i = starting; i < len; i++)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+}
