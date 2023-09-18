71e818a887e214f4c68e45efa2d78bf6c93960cf
root <elrayah.alsanhory@gmail.com>
2023-09-18 12:31:55 -0700

    first pointer eersize

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * reset_to_98 - the entry for the program
+ * @n: pointer to variable to change
+ * Return: 0 on sucess
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
