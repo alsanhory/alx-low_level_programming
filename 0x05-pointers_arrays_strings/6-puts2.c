21f685a0a833b677742f5f527e607415d0e29610
root <elrayah.alsanhory@gmail.com>
2023-09-18 15:51:05 -0700

    put another character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,12 +7,15 @@
  */
 void puts2(char *str)
 {
-	int len = 0;
+	int len = 0, i;
 
 	while (str[len] != '\0')
 	{
-		putchar(str[len]);
-		len  +=  2;
+		len++;
+	}
+	for (i = 0; i < len; i += 2)
+	{
+		putchar(str[i]);
 	}
 	putchar('\n');
 }
