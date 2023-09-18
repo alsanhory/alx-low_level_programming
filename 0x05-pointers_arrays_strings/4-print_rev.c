3cb158e04630358e81a468c380971237d6ac227d
root <elrayah.alsanhory@gmail.com>
2023-09-18 13:36:49 -0700

    trying to check the reserve characters

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * print_rev - the entry for the program
+ * @str: pointer to variable to change
+ * Return: the number of len on sucess
+ */
+void print_rev(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	while (len >= 0)
+	{
+		putchar(str[len]);
+		len--;
+	}
+	putchar('\n');
+}
