79fa92829914eeb90d1a9363fded5096b392e759
root <elrayah.alsanhory@gmail.com>
2023-09-21 19:52:20 -0700

    array swap

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,14 +2,14 @@
 #include<stdio.h>
 /**
  * reverse_array - the entry for the program
- * @s1: pointer to variable
- * @s2: staring point for the array
+ * @a: pointer to variable
+ * @n: staring point for the array
  * Return: the number of len on sucess
  */
 void reverse_array(int *a, int n)
 {
 	int firstpoint = 0;
-	int theend = n -1;
+	int theend = n - 1;
 	int tmpswap;
 
 	while (firstpoint < theend)
