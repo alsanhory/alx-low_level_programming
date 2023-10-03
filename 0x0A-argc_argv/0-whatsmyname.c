1308509752e2ac68739d9cb38fad40e8a5d7eb16
root <elrayah.alsanhory@gmail.com>
2023-10-02 19:22:00 -0700

    what is my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * main - the main function
+ * @argc: the arugments count
+ * @argv: the vector array
+ * Return: 0 on sucess
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
