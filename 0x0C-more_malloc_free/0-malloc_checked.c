e1c100e2bad9c78513e68cc05554fa2b30f51dde
root <elrayah.alsanhory@gmail.com>
2023-10-08 02:59:45 -0700

    malloc check

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - the main function
+ * @b: the arugments count
+ * Return: 0 on sucess
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *c;
+
+	c = malloc(b);
+	if (c != NULL)
+	{
+		return (c);
+	}
+	else
+	{
+		return (98);
+	}
+
+}
