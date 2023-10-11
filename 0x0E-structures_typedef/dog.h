c1259d526d9c72b7199f98a6844e83c9a5ba0478
root <elrayah.alsanhory@gmail.com>
2023-10-10 19:00:10 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef mydog
+#define mydog
+/**
+ * struct dog - my dog
+ * @name: the name
+ * @age: the age
+ * @owner: the owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
