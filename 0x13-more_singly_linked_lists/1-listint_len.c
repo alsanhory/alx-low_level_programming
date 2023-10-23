2488613273c248a8c5d71dceeea78123a673d645
root <elrayah.alsanhory@gmail.com>
2023-10-23 07:55:17 -0700

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * listint_len - the function to print list
+ * @h: the pointer to
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
