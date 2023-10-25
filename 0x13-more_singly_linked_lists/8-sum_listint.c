78427630ce4269065d5afe80e58ebda30dddc5b7
root <elrayah.alsanhory@gmail.com>
2023-10-25 15:22:26 -0700

    8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,18 +1,12 @@
 #include "lists.h"
 /**
- * get_nodeint_at_index - the function to print list
+ * sum_listint - the function to print list
  * @head: the pointer to
- * @index: the index to look
  * Return: number of nodes
  */
 int sum_listint(listint_t *head)
 {
 	int sum = 0;
-
-	if (head == NULL)
-	{
-		return (0);
-	}
 
 	while (head != NULL)
 	{
