5f2c1da19b898e1a3a79b4aa465d1173611232d2
alsanhory <elrayah.alsasnhory@gmail.com>
2023-12-07 15:59:51 -0800

    noes

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_dlistint - prints all
+ * @h: pointer to head
+ * Return: nodes count
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *current = h;
+	size_t node_count = 0;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		node_count++;
+	}
+
+	return (node_count);
+}
